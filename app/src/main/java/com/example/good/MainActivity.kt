4f8ab396438b844aef82a7b5df43c18ebd656c51
Targino52 <jonnastargino@alu.ufc.br>
2024-06-14 09:09:13 -0300

    topbar componente

--- app/src/main/java/com/example/good/MainActivity.kt
+++ app/src/main/java/com/example/good/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.good.ui.theme.GoodTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +30,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             GoodTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    MyScreen(
                     )
                 }
             }
@@ -30,18 +38,43 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MyscreenPreview() {
     GoodTheme {
-        Greeting("Android")
+        MyScreen()
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyScreen() {
+    var nome by remember { mutableStateOf("") } // Estado para o nome
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Bom dia, $nome") }, // Título dinâmico
+
+            )
+        },
+        floatingActionButton = { /* ... seu FAB ... */ }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            OutlinedTextField(
+                value = nome,
+                onValueChange = { nome = it },
+                label = { Text("Digite seu nome") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            )
+        }
     }
 }
